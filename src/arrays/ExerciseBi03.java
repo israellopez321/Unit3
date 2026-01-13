31761bcda2fc2fa0da982bd3af4747043829c9c6
darkx <israel.lopez@iesnervion.com>
2026-01-13 21:11:48 +0100

    a

---
+++ src/arrays/ExerciseBi03.java
@@ -0,0 +1,26 @@
+package arrays;
+
+import java.util.Scanner;
+
+public class ExerciseBi03 {
+	
+	public static void main(String[] args) {
+			
+		Scanner sc = new Scanner(System.in);
+		
+		//Preguntamos por las filas 
+		System.out.print("Introduzca el número de filas de la tabla:  ");
+		int filas = sc.nextInt();
+		
+		//Preguntamos por las columnas
+		System.out.print("Introduzca el número de columnas de la tabla: ");
+		int columnas = sc.nextInt();
+		
+		//Creamos array bidimensional
+		
+		
+		
+		
+		
+	}
+}
