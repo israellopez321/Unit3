57a3b73cf66776a98c8c2e5d9bd67f62b9dcc3a2
darkx <israel.lopez@iesnervion.com>
2026-03-16 21:30:05 +0100

    Collectioon ejercicio 2

--- src/collections/Ejercicio02.java
+++ src/collections/Ejercicio02.java
@@ -1,5 +1,6 @@
 package collections;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
@@ -11,12 +12,41 @@ public class Ejercicio02 {
 		Random rand =  new Random();
 		
 		//Creamos la lista 
-		List<integer> numerosRandom = new ArrayList<>();	
+		List<Integer> numerosRandom = new ArrayList<>();	
+		
+		//Rellenamos la lisa aleatoriamente
+		for(int indice = 0; indice <= rand.nextInt(5,11); indice++ ) {
+			
+			numerosRandom.add(rand.nextInt(0,101));
+			
+		}
 		
 		
+		int suma = 0;
+		int media;
 		
+		  for (int num : numerosRandom) {
+	            suma += num;
+	        }
+		  
+		  media = suma + numerosRandom.size();
 		
-		
+		  int max = numerosRandom.get(0);
+		  int min = numerosRandom.get(0);
+
+		  for (int num : numerosRandom) {
+		      if (num > max) {
+		          max = num;
+		      }
+		      if (num < min) {
+		          min = num;
+		      }
+		  }
+		  
+		System.out.println(suma);
+		System.out.println(media);
+		System.out.println(max);
+		System.out.println(min);
 		
 		
 		
